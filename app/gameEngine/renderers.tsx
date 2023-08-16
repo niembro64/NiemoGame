38c1aed172c8c4188b86f127bbbd1a16d159c292
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-08-16 00:32:04 -0400

    tsx

---
+++ app/gameEngine/renderers.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react-native/no-color-literals */
+import React, { PureComponent } from "react"
+import { StyleSheet, View } from "react-native"
+
+const RADIUS = 20
+
+class Finger extends PureComponent {
+  render() {
+    // @ts-ignore
+    const x = this.props.position[0] - RADIUS / 2
+    // @ts-ignore
+    const y = this.props.position[1] - RADIUS / 2
+    return <View style={[styles.finger, { left: x, top: y }]} />
+  }
+}
+
+const styles = StyleSheet.create({
+  finger: {
+    backgroundColor: "pink",
+    borderColor: "#CCC",
+    borderRadius: RADIUS * 2,
+    borderWidth: 4,
+    height: RADIUS * 2,
+    position: "absolute",
+    width: RADIUS * 2,
+  },
+})
+
+export { Finger }
--- app/screens/DemoShowroomScreen/DemoShowroomScreen.tsx
+++ app/screens/DemoShowroomScreen/DemoShowroomScreen.tsx
@@ -1,80 +1,16 @@
 /* eslint-disable react-native/no-color-literals */
 /* eslint-disable react-native/no-inline-styles */
-import { RouteProp, useRoute } from "@react-navigation/native"
-import React, { FC, useEffect, useRef, useState } from "react"
-import { SectionList, StatusBar, View, ViewStyle } from "react-native"
-import { DrawerLayout } from "react-native-gesture-handler"
-import { Screen } from "../../components"
-import { DemoTabParamList, DemoTabScreenProps } from "../../navigators/DemoNavigator"
-import * as Demos from "./demos"
 import { colors } from "app/theme"
+import React, { FC } from "react"
+import { StatusBar, View, ViewStyle } from "react-native"
 import { GameEngine } from "react-native-game-engine"
-import { MoveFinger } from "../../gameEngine/systems"
+import { Screen } from "../../components"
 import { Finger } from "../../gameEngine/renderers"
-
-const slugify = (str) =>
-  str
-    .toLowerCase()
-    .trim()
-    .replace(/[^\w\s-]/g, "")
-    .replace(/[\s_-]+/g, "-")
-    .replace(/^-+|-+$/g, "")
+import { MoveFinger } from "../../gameEngine/systems"
+import { DemoTabScreenProps } from "../../navigators/DemoNavigator"
 
 export const DemoShowroomScreen: FC<DemoTabScreenProps<"DemoShowroom">> =
   function DemoShowroomScreen(_props) {
-    const [open, setOpen] = useState(false)
-    const timeout = useRef<ReturnType<typeof setTimeout>>()
-    const drawerRef = useRef<DrawerLayout>()
-    const listRef = useRef<SectionList>()
-
-    const route = useRoute<RouteProp<DemoTabParamList, "DemoShowroom">>()
-    const params = route.params
-
-    // handle Web links
-    React.useEffect(() => {
-      if (route.params) {
-        const demoValues = Object.values(Demos)
-        const findSectionIndex = demoValues.findIndex(
-          (x) => x.name.toLowerCase() === params.queryIndex,
-        )
-        let findItemIndex = 0
-        if (params.itemIndex) {
-          try {
-            findItemIndex =
-              demoValues[findSectionIndex].data.findIndex(
-                (u) => slugify(u.props.name) === params.itemIndex,
-              ) + 1
-          } catch (err) {
-            console.error(err)
-          }
-        }
-        handleScroll(findSectionIndex, findItemIndex)
-      }
-    }, [route])
-
-    const toggleDrawer = () => {
-      if (!open) {
-        setOpen(true)
-        drawerRef.current?.openDrawer({ speed: 2 })
-      } else {
-        setOpen(false)
-        drawerRef.current?.closeDrawer({ speed: 2 })
-      }
-    }
-
-    const handleScroll = (sectionIndex: number, itemIndex = 0) => {
-      listRef.current.scrollToLocation({
-        animated: true,
-        itemIndex,
-        sectionIndex,
-      })
-      toggleDrawer()
-    }
-
-    useEffect(() => {
-      return () => timeout.current && clearTimeout(timeout.current)
-    }, [])
-
     return (
       <Screen preset="fixed" safeAreaEdges={["top"]} contentContainerStyle={$screenContainer}>
         <View
